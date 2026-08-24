f42ac62773d8421346a29664fffca6453f692a6a
CaiqueMMarques <caiquedemelo24@gmail.com>
2026-08-24 14:00:39 -0300

    adicionado card de molhos e bebidas

--- card.js
+++ card.js
@@ -423,6 +423,65 @@ sections.forEach(section => {
                             </div>
                         </div>
             `
+        }else if (selectProduct.category === "Molhos" || selectProduct.category === "Bebidas") {
+            main.innerHTML = `
+            <div class="card-info">
+                <div class="exit-the-card">
+                    <button class="button-exit">
+                        <svg viewBox="-1.92 -1.92 35.84 35.84" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g id="icomoon-ignore"> </g>
+                                <path
+                                    d="M13.114 2.887c-7.243 0-13.114 5.871-13.114 13.113s5.871 13.113 13.114 13.113c7.242 0 13.112-5.871 13.112-13.113s-5.87-13.113-13.112-13.113zM13.114 28.064c-6.653 0-12.065-5.412-12.065-12.064s5.412-12.063 12.065-12.063c6.652 0 12.063 5.412 12.063 12.063s-5.411 12.064-12.063 12.064z">
+                                </path>
+                                <path
+                                    d="M12.318 10.363l-0.742-0.742-6.379 6.379 6.379 6.379 0.742-0.742-5.113-5.113h12.726v-1.049h-12.726z">
+                                </path>
+                            </g>
+                        </svg>
+                    </button>
+                    <h2 class="card-title">${selectProduct.name}</h2>
+                </div>
+                <p class="card-description">${selectProduct.description ?? ""}</p>
+            </div>
+            <div class="card-opinion">
+                <input class="opinion-input" type="text" placeholder="Digite observações aqui (opcional)...">
+            </div>
+            <div class="card-fixed">
+                <div class="card-price">
+                    <b class="price-to-card">R$ ${selectProduct.price.toFixed(2)}</b>
+                    <div class="button-more-or-less">
+                        <button class="button-subtrair btn">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                                <path
+                                    d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z" />
+                            </svg>
+                        </button>
+                        <span class="number-bottom">1</span>
+                        <button class="button-somar btn">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                                <path
+                                    d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="button-add-cart">
+                    <button class="add-cart"><svg class="svg-cart" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                            <path
+                                d="M24 48C10.7 48 0 58.7 0 72C0 85.3 10.7 96 24 96L69.3 96C73.2 96 76.5 98.8 77.2 102.6L129.3 388.9C135.5 423.1 165.3 448 200.1 448L456 448C469.3 448 480 437.3 480 424C480 410.7 469.3 400 456 400L200.1 400C188.5 400 178.6 391.7 176.5 380.3L171.4 352L475 352C505.8 352 532.2 330.1 537.9 299.8L568.9 133.9C572.6 114.2 557.5 96 537.4 96L124.7 96L124.3 94C119.5 67.4 96.3 48 69.2 48L24 48zM208 576C234.5 576 256 554.5 256 528C256 501.5 234.5 480 208 480C181.5 480 160 501.5 160 528C160 554.5 181.5 576 208 576zM432 576C458.5 576 480 554.5 480 528C480 501.5 458.5 480 432 480C405.5 480 384 501.5 384 528C384 554.5 405.5 576 432 576z" />
+                        </svg>
+                        <span class="cart-text">Adicionar</span></button>
+                </div>
+            </div>
+            `
+
         }
 
         divCard.appendChild(main);
